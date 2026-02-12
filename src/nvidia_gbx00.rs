ab5247311500d46e78627212e78633dc27d23181
Jakhongir Rakhmonov <jrakhmonov@nvidia.com>
2026-02-12 16:13:54 +0000

    Change the target parameter to empty for GB200 BMC UpdateService

--- src/nvidia_gbx00.rs
+++ src/nvidia_gbx00.rs
@@ -1096,7 +1096,10 @@ impl Redfish for Bmc {
         self.s.get_evidence(url).await
     }
 
-    async fn set_host_privilege_level(&self, level: HostPrivilegeLevel) -> Result<(), RedfishError> {
+    async fn set_host_privilege_level(
+        &self,
+        level: HostPrivilegeLevel,
+    ) -> Result<(), RedfishError> {
         self.s.set_host_privilege_level(level).await
     }
 
@@ -1337,18 +1340,17 @@ impl UpdateParameters {
     pub fn new(component: ComponentType) -> UpdateParameters {
         let targets = match component {
             ComponentType::Unknown => None,
-            _ => Some(vec![match component {
-                ComponentType::BMC => "/redfish/v1/Chassis/BMC_0".to_string(),
-                ComponentType::EROTBMC => "/redfish/v1/Chassis/HGX_ERoT_BMC_0".to_string(),
-                ComponentType::EROTBIOS => {
-                    "/redfish/v1/UpdateService/FirmwareInventory/EROT_BIOS_0".to_string()
-                }
-                ComponentType::HGXBMC | ComponentType::UEFI => {
-                    "/redfish/v1/Chassis/HGX_Chassis_0".to_string()
-                }
-                _ => "unreachable".to_string(),
-            }]),
+            ComponentType::BMC => Some(vec![]),
+            ComponentType::EROTBMC => Some(vec!["/redfish/v1/Chassis/HGX_ERoT_BMC_0".to_string()]),
+            ComponentType::EROTBIOS => Some(vec![
+                "/redfish/v1/UpdateService/FirmwareInventory/EROT_BIOS_0".to_string(),
+            ]),
+            ComponentType::HGXBMC | ComponentType::UEFI => {
+                Some(vec!["/redfish/v1/Chassis/HGX_Chassis_0".to_string()])
+            }
+            _ => Some(vec!["unreachable".to_string()]),
         };
+
         UpdateParameters {
             targets,
             force_update: true,
@@ -1364,10 +1366,7 @@ mod tests {
     fn test_update_parameters_targets_all_variants() {
         let cases: Vec<(ComponentType, Option<Vec<String>>)> = vec![
             (ComponentType::Unknown, None),
-            (
-                ComponentType::BMC,
-                Some(vec!["/redfish/v1/Chassis/BMC_0".to_string()]),
-            ),
+            (ComponentType::BMC, Some(vec![])),
             (
                 ComponentType::EROTBMC,
                 Some(vec!["/redfish/v1/Chassis/HGX_ERoT_BMC_0".to_string()]),
